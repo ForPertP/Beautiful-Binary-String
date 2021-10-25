11fe76ec3fc647fe66de6ac2028fa190725a492d
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-10-26 04:08:30 +0900

    Update Beautiful-Binary-String.cpp

--- Beautiful-Binary-String.cpp
+++ Beautiful-Binary-String.cpp
@@ -1,1 +1,16 @@
+int beautifulBinaryString(string b)
+{
+    int result = 0;
+    
+    for (int i = 0; i < b.size(); ++i)
+    {
+        if (b.substr(i, 3) == "010")
+        {
+            result++;
+            i += 2;
+        }
+    }    
+
+    return result;
+}
 
