93316d261b668cd74613cefb804b273ed5c99ed9
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-05-26 05:06:03 +0900

    Update Beautiful-Binary-String.cpp

--- Beautiful-Binary-String.cpp
+++ Beautiful-Binary-String.cpp
@@ -1,13 +1,4 @@
-int beautifulBinaryString(string b)
-{
-    int result = 0;
-    
-    for (int i = 0; i < b.size(); ++i)
-    {
-        if (b.substr(i, 3) == "010")
-        {
-            result++;
-            i += 2;#include <bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -37,3 +28,46 @@ int beautifulBinaryString(string b)
     return result;
 }
 
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string b;
+    getline(cin, b);
+
+    int result = beautifulBinaryString(b);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
