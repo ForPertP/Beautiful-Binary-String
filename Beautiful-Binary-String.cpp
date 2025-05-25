ba165e65245426543426c84e8aa3868ff3862ef5
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-05-26 05:05:51 +0900

    Update Beautiful-Binary-String.cpp

--- Beautiful-Binary-String.cpp
+++ Beautiful-Binary-String.cpp
@@ -7,6 +7,29 @@ int beautifulBinaryString(string b)
         if (b.substr(i, 3) == "010")
         {
             result++;
+            i += 2;#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'beautifulBinaryString' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING b as parameter.
+ */
+
+int beautifulBinaryString(string b)
+{
+    int result = 0;
+    
+    for (int i = 0; i < b.size(); ++i)
+    {
+        if (b.substr(i, 3) == "010")
+        {
+            result++;
             i += 2;
         }
     }    
