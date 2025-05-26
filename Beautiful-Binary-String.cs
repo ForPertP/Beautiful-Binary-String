53095774c8ff1fe6178bf824b69b18edcc7fb579
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-05-27 05:44:58 +0900

    Update Beautiful-Binary-String.cs

--- Beautiful-Binary-String.cs
+++ Beautiful-Binary-String.cs
@@ -1,1 +1,60 @@
-Beautiful-Binary-String
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'beautifulBinaryString' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING b as parameter.
+     */
+
+    public static int beautifulBinaryString(string b)
+    {
+        int result = 0;
+        
+        for (int i = 0; i <= b.Length - 3; ++i)
+        {
+            if (b.Substring(i, 3) == "010")
+            {
+                result++;
+                i += 2;
+            }
+        }    
+
+        return result;
+    }
+}
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string b = Console.ReadLine();
+
+        int result = Result.beautifulBinaryString(b);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
