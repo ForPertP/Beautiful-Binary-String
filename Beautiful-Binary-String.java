0e630671f63e45c87b2b1b31e8a95cb00fe3019b
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-05-28 04:51:28 +0900

    Update Beautiful-Binary-String.java

--- Beautiful-Binary-String.java
+++ Beautiful-Binary-String.java
@@ -1,1 +1,56 @@
-Beautiful-Binary-String
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'beautifulBinaryString' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING b as parameter.
+     */
+
+    public static int beautifulBinaryString(String b) {
+        int result = 0;
+        
+        for (int i = 0; i <= b.length() - 3; ++i)
+        {
+            if (b.substring(i, i + 3).equals("010"))
+            {
+                result++;
+                i += 2;
+            }
+        }    
+
+        return result;
+    }
+}
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String b = bufferedReader.readLine();
+
+        int result = Result.beautifulBinaryString(b);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
